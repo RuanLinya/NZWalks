7be117d640f781c8842cc50ce57879182eca0a8c
Linya Ruan <ruanlinya@gmail.com>
2025-12-08 22:20:16 +0100

    added Navigation

--- Models/DTO/WalkDto.cs
+++ Models/DTO/WalkDto.cs
@@ -10,7 +10,11 @@
         public double LengthInkm { get; set; }
         public string? WalkImageUrl { get; set; }
 
-        public Guid DifficultyId { get; set; }
-        public Guid RegionId { get; set; }
+        //public Guid DifficultyId { get; set; }
+        //public Guid RegionId { get; set; }
+
+        
+        public RegionDto Region { get; set; }
+        public DifficultyDto Difficulty { get; set; }
     }
 }
