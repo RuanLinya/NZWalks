70ca2455c51bb115fc47de69096653a5ec069de9
Linya Ruan <ruanlinya@gmail.com>
2026-01-20 18:01:23 +0100

    Image Upload

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+
+        // POST API/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload (request);
+
+            if (ModelState.IsValid)
+            {
+                // convert DTO to domain model 
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = request.FileName,
+                    FileDescription = request.FileDescription,
+
+                };
+
+                // User repository to upload image
+                await imageRepository.Upload(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png"};
+            if(!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("File", "Unsupported file format. Only .jpg, .jpeg, .png are allowed.");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("File", "File size exceeds the 10MB limit.");
+            }
+
+
+        }
+
+    }
+}
---
+++ Repositories/LocalImageRepository.cs
@@ -0,0 +1,40 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using System.Diagnostics;
+
+namespace NZWalks.API.Repositories
+{
+    public class LocalImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly NZWalksDbContext dbContext;    
+
+        public LocalImageRepository(IWebHostEnvironment webHostEnvironment,
+           IHttpContextAccessor httpContextAccessor, NZWalksDbContext dbContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+            this.dbContext = dbContext;
+        }
+        public async Task<Image> Upload(Image image)
+        {
+           var localFilePath = Path.Combine(webHostEnvironment.ContentRootPath, "Images", 
+               $"{image.FileName}{image.FileExtension}");
+            // Upload the image to local Path
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            // Http : Local Path mapping
+            var urlFilePath = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}{httpContextAccessor.HttpContext.Request.PathBase}/Images/{image.FileName}{image.FileExtension}";
+            image.FilePath = urlFilePath;
+
+            // Add image to the Images table
+            await dbContext.Images.AddAsync(image); 
+            await dbContext.SaveChangesAsync();
+
+            return image;
+
+        }
+    }
+}
